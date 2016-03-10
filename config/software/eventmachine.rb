615339ee075ac28e4a6de1c0db6ff4141b79cef8
Sean Porter <portertech@gmail.com>
2016-03-09 18:42:11 -0800

    eventmachine software, use version for branch

--- config/software/eventmachine.rb
+++ config/software/eventmachine.rb
@@ -2,18 +2,22 @@ name "eventmachine"
 
 dependency "ruby"
 
-source git: 'https://github.com/portertech/eventmachine.git'
+source git: "git@github.com:portertech/eventmachine.git"
+
+default_version "hotfix/aix-compile-backport"
+
+version "hotfix/aix-compile-backport" do
+  source git: "git@github.com:portertech/eventmachine.git"
+end
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  command 'git checkout hotfix/aix-compile-backport', env: env
+  command "gem install rake-compiler", env: env
 
-  command 'gem install rake-compiler', env: env
+  command "rake clean", env: env
+  command "rake compile", env: env
+  command "rake gem", env: env
 
-  command 'rake clean', env: env
-  command 'rake compile', env: env
-  command 'rake gem', env: env
-
-  command 'gem install pkg/eventmachine-1.0.9.1.gem', env: env
+  command "gem install pkg/eventmachine-1.0.9.1.gem", env: env
 end
