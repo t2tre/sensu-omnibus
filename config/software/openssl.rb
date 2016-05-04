dfff37bca2e5faab7bc61f45c3546b0d8583d511
Justin Kolberg <amd.prophet@gmail.com>
2016-05-04 15:01:11 -0700

    bump openssl to 1.0.1t, https://www.openssl.org/news/secadv/20160503.txt

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -27,7 +27,7 @@ dependency "makedepend" unless aix? || windows?
 dependency "patch" if solaris_10?
 dependency "openssl-fips" if fips_enabled
 
-default_version "1.0.1s"
+default_version "1.0.1t"
 
 # OpenSSL source ships with broken symlinks which windows doesn't allow.
 # Skip error checking.
@@ -38,6 +38,7 @@ source url: "https://www.openssl.org/source/openssl-#{version}.tar.gz", extract:
 version("1.0.2g") { source md5: "f3c710c045cdee5fd114feb69feba7aa" }
 version("1.0.1s") { source md5: "562986f6937aabc7c11a6d376d8a0d26" }
 version("1.0.1r") { source md5: "1abd905e079542ccae948af37e393d28" }
+version("1.0.1t") { source md5: "9837746fcf8a6727d46d22ca35953da1" }
 
 relative_path "openssl-#{version}"
 
