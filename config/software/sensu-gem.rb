60624ba995bbb0ec4f898c2c1d9cb00f0768c4ef
Cameron Johnston <cameron@rootdown.net>
2016-08-23 14:37:03 -0600

    bump sensu-plugin from 1.3.0 to 1.4.2

--- config/software/sensu-gem.rb
+++ config/software/sensu-gem.rb
@@ -27,7 +27,7 @@ build do
       " --no-ri --no-rdoc", env: env
 
   gem "install sensu-plugin" \
-      " --version '1.3.0'" \
+      " --version '1.4.2'" \
       " --no-ri --no-rdoc", env: env
 
   share_dir = File.join(install_dir, "embedded", "share", "sensu")
