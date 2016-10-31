72a150a81418abd0bf97c15a95b10f794c3930c2
Justin Kolberg <amd.prophet@gmail.com>
2016-10-30 17:27:41 -0700

    [freebsd] run config validation under sensu user

--- site-cookbooks/omnibus_sensu/recipes/default.rb
+++ site-cookbooks/omnibus_sensu/recipes/default.rb
@@ -13,6 +13,7 @@ end
 omnibus_build "sensu" do
   project_dir "/home/vagrant/sensu"
   log_level :internal
+  build_user "root"
   environment({
     "SENSU_VERSION" => node["omnibus_sensu"]["build_version"],
     "BUILD_NUMBER" => node["omnibus_sensu"]["build_iteration"]
