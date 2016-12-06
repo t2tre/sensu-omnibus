742ae2155f5cd24b58476fd94057f1d2d8491320
Justin Kolberg <amd.prophet@gmail.com>
2016-12-06 10:37:02 -0800

    [package-files] avoid creating files with postinst script, use package_extra_file

--- config/software/sensu-gem.rb
+++ config/software/sensu-gem.rb
@@ -31,30 +31,61 @@ build do
   bin_dir = File.join(install_dir, "bin")
   embedded_bin_dir = File.join(install_dir, "embedded", "bin")
 
+  if freebsd?
+    etc_dir = "/usr/local/etc"
+    usr_bin_dir = "/usr/local/bin"
+  else
+    etc_dir = "/etc"
+    usr_bin_dir = "/usr/bin"
+  end
+
   # make directories
   mkdir("#{install_dir}/bin")
-  mkdir("#{share_dir}/etc/sensu")
-  mkdir("#{share_dir}/etc/default")
-  mkdir("#{share_dir}/etc/logrotate.d")
-  mkdir("#{share_dir}/etc/rc.d")
-  mkdir("#{share_dir}/lib/svc/manifest/site")
+  mkdir("#{etc_dir}/sensu")
+  mkdir("#{etc_dir}/sensu/conf.d")
+  mkdir("#{etc_dir}/sensu/extensions")
+  mkdir("#{etc_dir}/sensu/plugins")
+  mkdir("/var/log/sensu")
+  mkdir("/var/run/sensu")
+  mkdir("/var/cache/sensu")
+  mkdir("#{etc_dir}/logrotate.d")
+  mkdir("/lib/svc/manifest/site") if solaris?
+  mkdir("#{etc_dir}/default") unless rhel? || debian? # if directory doesn't exist would be better
 
-  # config.json.example
-  copy("#{files_dir}/config.json.example", "#{share_dir}/etc/sensu")
+  # .keep files to ensure all directories have at least one file
+  touch("#{etc_dir}/sensu/conf.d/.keep")
+  touch("#{etc_dir}/sensu/extensions/.keep")
+  touch("#{etc_dir}/sensu/plugins/.keep")
+  touch("/var/log/sensu/.keep")
+  touch("/var/run/sensu/.keep")
+  touch("/var/cache/sensu/.keep")
 
-  # default file
-  copy("#{files_dir}/default/sensu", "#{share_dir}/etc/default")
-
-  # sensu-install
+  # sensu-install (in omnibus bin dir)
   copy("#{files_dir}/bin/sensu-install", bin_dir)
+  copy("#{files_dir}/bin/sensu-install", "#{usr_bin_dir}/sensu-install")
   command("chmod +x #{bin_dir}/sensu-install")
+  command("chmod +x #{usr_bin_dir}/sensu-install")
+
+  # misc files
+  copy("#{files_dir}/config.json.example", "#{etc_dir}/sensu/config.json.example")
+  copy("#{files_dir}/default/sensu", "#{etc_dir}/default/sensu")
+  copy("#{files_dir}/logrotate.d/sensu", "#{etc_dir}/logrotate.d/sensu")
+
+  # add extra package files (files outside of /opt/sensu)
+  project.extra_package_file("#{etc_dir}/sensu/config.json.example")
+  project.extra_package_file("#{etc_dir}/default/sensu")
+  project.extra_package_file("#{etc_dir}/logrotate.d/sensu")
+  project.extra_package_file("#{usr_bin_dir}/sensu-install")
+  project.extra_package_file("#{etc_dir}/sensu/conf.d/.keep")
+  project.extra_package_file("#{etc_dir}/sensu/extensions/.keep")
+  project.extra_package_file("#{etc_dir}/sensu/plugins/.keep")
+  project.extra_package_file("/var/log/sensu/.keep")
+  project.extra_package_file("/var/run/sensu/.keep")
+  project.extra_package_file("/var/cache/sensu/.keep")
 
   # sensu-service
   copy("#{files_dir}/bin/sensu-service", bin_dir)
   command("chmod +x #{bin_dir}/sensu-service")
-
-  # logrotate.d config
-  copy("#{files_dir}/logrotate.d/sensu", "#{share_dir}/etc/logrotate.d")
 
   # determine which service manager to use
   platform = ohai["platform"]
@@ -65,7 +96,7 @@ build do
   # service wrappers
   service_dir = Helpers::directory_for_service(platform_family, service_manager)
 
-  # copy the sensu service files to their destination
+  # render the sensu service templates to their destination
   Helpers::services(service_manager).each do |sensu_service|
     filename = Helpers::filename_for_service(service_manager, sensu_service)
     destination = File.join(service_dir, filename)
@@ -81,9 +112,6 @@ build do
     erb(options)
     project.extra_package_file(destination)
   end
-
-  # sensu manifest (solaris)
-  copy("#{files_dir}/smf/sensu-client.xml", "#{share_dir}/lib/svc/manifest/site")
 
   # make symlinks
   link("#{embedded_bin_dir}/sensu-client", "#{bin_dir}/sensu-client")
