7e6ca5c23ae9a12f9874d043c4c11cbea8bd3ab2
Cameron Johnston <cameron@rootdown.net>
2016-12-09 16:12:23 -0700

    update travis.yml and build script

--- build.sh
+++ build.sh
@@ -2,22 +2,20 @@
 
 set -ev
 
-echo "Beginning build for ${BUILD_PLATFORM}..."
-
 if [ "x${BUILD_PLATFORM}" == "x" ]; then
     echo "BUILD_PLATFORM not set, exiting"
     exit
 fi
 
+echo "Beginning build for ${BUILD_PLATFORM}..."
+
 OMNIBUS_COMMIT=`git rev-parse HEAD`
 
 if [ `git describe --tags --exact-match $OMNIBUS_COMMIT` ]; then
-    TAG_VERSION=`git describe --abbrev=0 --tags | awk -F'-' '{print $1}' | sed 's/v//g'`
-    TAG_ITERATION=`git describe --abbrev=0 --tags | awk -F'-' '{print $2}'`
-    export SENSU_VERSION=$TAG_VERSION
-    export BUILD_NUMBER=$TAG_ITERATION
+    export SENSU_VERSION=`git describe --abbrev=0 --tags | awk -F'-' '{print $1}' | sed 's/v//g'`
+    export BUILD_NUMBER=`git describe --abbrev=0 --tags | awk -F'-' '{print $2}'`
     export KITCHEN_LOCAL_YAML=.kitchen.cloud.yml
-    echo "Build artifact will be published as ${version}-${iteration}"
+    echo "Building ${BUILD_PLATFORM} ${SENSU_VERSION}-${BUILD_NUMBER}"
     bundle exec rake kitchen:default-$BUILD_PLATFORM
 else
     echo "Commit ${OMNIBUS_COMMIT} is not tagged, exiting."
