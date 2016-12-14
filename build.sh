dd8a79376f05dd3224043ef7492e09ff9a24819c
Cameron Johnston <cameron@rootdown.net>
2016-12-14 12:33:23 -0700

    completely replace kitchen.yml with kitchen.cloud.yml

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ OMNIBUS_COMMIT=`git rev-parse HEAD`
 if [ `git describe --tags --exact-match $OMNIBUS_COMMIT` ]; then
     export SENSU_VERSION=`git describe --abbrev=0 --tags | awk -F'-' '{print $1}' | sed 's/v//g'`
     export BUILD_NUMBER=`git describe --abbrev=0 --tags | awk -F'-' '{print $2}'`
-    export KITCHEN_LOCAL_YAML=.kitchen.cloud.yml
     echo "Building ${BUILD_PLATFORM} ${SENSU_VERSION}-${BUILD_NUMBER}"
     bundle exec rake kitchen:default-$BUILD_PLATFORM
 else
