3a545e6786d21a9e04dc7e8de3029e4ae5f80f22
Justin Kolberg <amd.prophet@gmail.com>
2017-02-07 10:55:09 -0800

    Revert "[travis] refactor build.sh to handle commits with multiple tags"
    This reverts commit a283c023c8442b881806537af09a1b1b23572025.

--- build.sh
+++ build.sh
@@ -7,29 +7,27 @@ if [ "x${BUILD_PLATFORM}" == "x" ]; then
     exit 2
 fi
 
-OMNIBUS_COMMIT=$(git rev-parse HEAD)
+OMNIBUS_COMMIT=`git rev-parse HEAD`
 
-if [ "$(git describe --tags --exact-match "$OMNIBUS_COMMIT")" ]; then
-    # sort=-v:refname will reverse sort tags, treating them as versions, on git 2.0 and later
-    OMNIBUS_TAG=$(git tag -l --points-at HEAD --sort=-v:refname | awk 'NR==1{print $1}')
-    SENSU_VERSION=$(echo "$OMNIBUS_TAG" | awk -F'-' '{print $1}' | sed 's/v//g')
-    BUILD_NUMBER=$(echo "$OMNIBUS_TAG" | awk -F'-' '{print $2}')
+if [ `git describe --tags --exact-match $OMNIBUS_COMMIT` ]; then
+    TAGS=`git tag -l --points-at HEAD`
+    TAG_COUNT=`echo $TAGS | tr '[[:space:]]' '\n' | wc -l`
 
-    export SENSU_VERSION BUILD_NUMBER
-
-    if [[ "x$SENSU_VERSION" != "x" ]] && [[ "x$BUILD_NUMBER" != "x" ]]; then
-       echo "============================ Building ${SENSU_VERSION}-${BUILD_NUMBER} on ${BUILD_PLATFORM} ============================"
-
-       if [[ "x$TRAVIS_WAIT" == "x" ]] ; then
-           bundle exec rake kitchen:default-"$BUILD_PLATFORM"
-       else
-           # shellcheck source=.travis/functions.sh
-           source "$TRAVIS_BUILD_DIR/.travis/functions.sh"
-           travis_wait "$TRAVIS_WAIT" bundle exec rake kitchen:default-"$BUILD_PLATFORM"
-       fi
-    else
-        echo "!!! Failed to parse tag \"$OMNIBUS_TAG\" into version ($SENSU_VERSION) and build number ($BUILD_NUMBER)"
+    # Please use unique commits when creating tags to trigger this build
+    if [[ "$TAG_COUNT" -ne "1" ]] ; then
+        echo "Error: Found multiple tags matching $OMNIBUS_COMMIT : $(echo $TAGS)"
         exit 2
+    fi
+
+    export SENSU_VERSION=`git describe --abbrev=0 --tags | awk -F'-' '{print $1}' | sed 's/v//g'`
+    export BUILD_NUMBER=`git describe --abbrev=0 --tags | awk -F'-' '{print $2}'`
+    echo "============================ Building ${SENSU_VERSION}-${BUILD_NUMBER} on ${BUILD_PLATFORM} ============================"
+
+    if [[ "x$TRAVIS_WAIT" == "x" ]] ; then
+        bundle exec rake kitchen:default-$BUILD_PLATFORM
+    else
+        source "$TRAVIS_BUILD_DIR/.travis/functions.sh"
+        travis_wait $TRAVIS_WAIT bundle exec rake kitchen:default-$BUILD_PLATFORM
     fi
 else
     echo "!!! Commit ${OMNIBUS_COMMIT} is not tagged, exiting."
