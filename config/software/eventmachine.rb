b35ea14d23282c9793372706bc7de97c42b2a9de
Cameron Johnston <cameron@sensu.io>
2017-02-20 14:35:26 -0700

    [software/eventmachine] add --with-ssl* compile options when building on windows

--- config/software/eventmachine.rb
+++ config/software/eventmachine.rb
@@ -28,15 +28,28 @@ build do
   command "gem install --no-document rake-compiler", env: env
   command "rake clean", env: env
 
+  windows_install_prefix = "C:\\opt\\sensu\\embedded"
+
   # disable C++ extensions so we don't need to compile them on platforms
   # we're only using pure_ruby with
   if aix? || solaris?
     patch source: "disable-extensions.patch", plevel: 1, env: patch_env
+  elsif windows?
+    command "rake compile -- --with-ssl-dir=#{windows_install_prefix}\\bin", env: env
   else
     command "rake compile", env: env
   end
 
   command "rake gem", env: env
 
-  command "gem install --no-document pkg/eventmachine-1.2.2.gem", env: env
+  compile_options = ''
+
+  if windows?
+    compile_options << " --with-ssl-dir=#{windows_install_prefix}\\bin"
+    compile_options << " --with-ssl-lib=#{windows_install_prefix}\\lib"
+    compile_options << " --with-ssl-include=#{windows_install_prefix}\\include"
+    compile_options << " --with-opt-include=#{windows_install_prefix}\\include"
+  end
+
+  command "gem install --no-document pkg/eventmachine-1.2.2.gem -- #{compile_options}", env: env
 end
