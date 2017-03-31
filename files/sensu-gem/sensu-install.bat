61acf2970945689a320c327191a999e35ac7878d
Cameron Johnston <cameron@sensu.io>
2017-03-31 09:00:52 -0600

    Update GEM_PATH in sensu-install.bat

--- files/sensu-gem/sensu-install.bat
+++ files/sensu-gem/sensu-install.bat
@@ -1,4 +1,4 @@
 PATH=C:\opt\sensu\embedded\bin;%PATH%
-GEM_PATH=C:\opt\sensu\embedded\lib\ruby\gems\2.3.0;%GEM_PATH%
+GEM_PATH=C:\opt\sensu\embedded\lib\ruby\gems\2.4.0;%GEM_PATH%
 
 C:\opt\sensu\embedded\bin\ruby C:\opt\sensu\embedded\bin\sensu-install %*
