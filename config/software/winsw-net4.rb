820614b7f3dbc4e196b44ac0e5b51b518791ae7f
Cameron Johnston <cameron@sensu.io>
2017-04-05 15:26:07 -0600

    [omnibus] remove commented code in winsw configs

--- config/software/winsw-net4.rb
+++ config/software/winsw-net4.rb
@@ -18,18 +18,6 @@ build do
   bin_dir = File.join(install_dir, "bin")
 
   if windows?
-    # framework = windows_arch_i386? ? "Framework" : "Framework64"
-    # mkdir(bin_dir)
-    # command_options = "/target:Clean;Build /p:Configuration=Release /p:PostBuildEvent="
-    # command("MSBuild.exe src/winsw.sln #{command_options}", env: env)
-    # copy("bin/Release/winsw.exe", "#{bin_dir}/winsw.exe")
     copy("WinSW.NET4.exe", "#{bin_dir}/winsw.exe")
   end
 end
-
-# files_dir = "#{Dir.pwd}\\assets\\msi\\files".gsub("/", '\\')
-
-# copy("bin/Release/winsw.exe", )
-# t.install_commands << "mkdir #{install_prefix}\\bin"
-# t.install_commands << "copy bin\\Release\\winsw.exe #{install_prefix}\\bin\\sensu-client.exe"
-# t.install_commands << "copy #{files_dir}\\sensu-client.exe.config #{install_prefix}\\bin\\sensu-client.exe.config"
